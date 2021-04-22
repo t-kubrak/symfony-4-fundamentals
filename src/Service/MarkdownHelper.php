9a23a4419c1f539ad74104498b4a09089e1c34b6
tkubrak <taras.kubrak@dataart.com>
2021-04-22 23:14:04 +0300

    feat: add logger; custom channel

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class MarkdownHelper
@@ -15,15 +16,24 @@ class MarkdownHelper
      * @var MarkdownInterface
      */
     private $markdown;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
 
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown)
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $logger)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
+        $this->logger = $logger;
     }
 
     public function parse(string $source): string
     {
+        if (stripos($source, 'bacon') !== false) {
+            $this->logger->info('They are talking about bacon again!');
+        }
+
         $item = $this->cache->getItem('markdown_'.md5($source));
 
         if (!$item->isHit()) {
